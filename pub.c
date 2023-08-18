85005d1dd87c9dd5458af2bb34ce2548a3f62158
Eugene Kim <eugenek0529@gmail.com>
2023-08-18 15:08:06 -0700

    pub msg and sub success

--- pub.c
+++ pub.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include "/opt/homebrew/include/mosquitto.h"
-
+#include <string.h>
 
 int main()
 {
+    /**
+     * First step is to initialize the mostquitto lib
+     * then create a object to certain channel to publish (broker)
+     * 
+    */
     int rc;
     struct mosquitto *mosq;
 
@@ -20,10 +25,29 @@ int main()
 
     printf("We are now connected to the broker\n");
 
+    /*
+     * Next is to create a topic and pulish my own payload 
+    */
 
+   const char *topic = "choice";
+   const char *payload = "payload_example";
+   int payloadLength = strlen(payload);
+   int qos = 0; // pubisher send meg once and doesnt wait for acknolegement
+   bool retain = false; // msg does not need to be saved
+   
 
+   int pub;
+   pub = mosquitto_publish(mosq, NULL, topic, payloadLength, payload,qos,retain);
 
-    
+   if (rc != 0)
+    {
+        printf("Failed to publish message\n");
+    }
+    else
+    {
+        printf("Message published successfully\n");
+    }
+
     // Clean up and disconnect
     mosquitto_disconnect(mosq);
     mosquitto_destroy(mosq);
