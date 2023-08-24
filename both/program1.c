a102f1c8aa2a2b6793e2e60442b3f4072c036fb6
Eugene Kim <eugenek0529@gmail.com>
2023-08-24 13:58:07 -0700

    old version structure deleted

--- both/program1.c
+++ both/program1.c
@@ -2,6 +2,17 @@
 #include "/opt/homebrew/include/mosquitto.h"
 #include <string.h>
 
+
+void on_connect(struct mosquitto *sub, void *obj, int rc2)
+{
+    printf("on_connect function is called\n");
+    if(rc2)
+    {
+        printf("Failed with result code: %d\n",rc2);
+    }
+    mosquitto_subscribe(sub,NULL, "topic",0);
+}
+
 void msg_sub(struct mosquitto *sub, void *obj, const struct mosquitto_message *msg)
 {
     printf("New message with topic %s: %s\n", msg->topic, (char *) msg->payload);
@@ -33,58 +44,8 @@ int main()
     }
 
     // function to continously receive msg from subed topic
+    mosquitto_connect_callback_set(sub,on_connect);
     mosquitto_message_callback_set(sub, msg_sub);
-
-
-
-
-    //
-    //
-    // OLD VERSION
-    //
-    //
-    char sessionEnd;
-    do{ 
-        printf("Start of new session\n");
-
-        // Prompt the user to choose what to play
-        char user;
-        
-        printf("Player(1) or Player(2)?: ");
-        do{
-            // input validation
-            user = getchar();
-        } while ((user != '1') || (user != '2'));
-
-
-
-        // sub loop start
-        mosquitto_loop_start(sub);
-        do{
-            // do what ever
-        } while (getchar() != 'q');
-        
-        // out the loop, stop sub-loop
-        mosquitto_loop_stop(sub,true);
-
-
-
-        // clear the connection for objects
-        mosquitto_disconnect(pub);
-        mosquitto_disconnect(sub);
-        mosquitto_destroy(pub);
-        mosquitto_destroy(sub);
-        mosquitto_lib_cleanup();
-
-        printf("\n\nDo you want to satrt a new session?: 1) yes 2) no");
-        sessionEnd = getchar();
-
-    } while (sessionEnd != '2'); //end of big-dowhile
-    //
-    // END of OLD VERSOIN
-    //
-    //
-
 
 
     // New version of total algorithm
@@ -94,12 +55,22 @@ int main()
     int session; // 1 - keep, 0 - end
     int quit;
     int role = 0; // 0 - pub, 1 - sub
+
+    // pub variables
+    const char *topic = "choice";
+    char *payload;
+    int payloadLength; // later use strlen(payload)
+    int qos = 0; // publisher send msg once and does not wait for ack
+    int published; // 0 - published, !0 - not published    
+    bool retain = false; // msg does not need to be saved
+
+
     do{
         printf("This is start of a new session\n");
         printf("Please choose your role: 0) pub 1) sub: ");
         do {
             scanf("%d", &role);
-        } while ((role != 0) || (role != 1));
+        } while ((role != 0) && (role != 1));
 
         // now the role is choosed
         do{
@@ -107,10 +78,30 @@ int main()
             if(role == 0)
             {
                 // pub
+                printf("Please enter your choice: " );
+                scanf("%c",payload);
+                
+                payloadLength = strlen(payload);
+
+                published = mosquitto_publish(pub,NULL,topic,payloadLength,payload,qos,retain);
+
+                if(published != 0)
+                {
+                    printf("Failed to publish message\n");
+                    break;
+                }
+                else 
+                {
+                    printf("Message published successfully\n");
+                    continue;
+                }
+
             }
             else if(role == 1)
             {
                 // sub
+                
+
             }
 
             // out here when task is done, and update role
